dd36cdc2d7883567c843778781bb60dd46e25324
Anuranga <jayan.anuranga@gmail.com>
2023-04-16 06:58:39 +0530

    vendor folder

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -19,12 +19,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Stellar Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="{{asset('vendors/simple-line-icons/css/simple-line-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('vendors/flag-icon-css/css/flag-icon.min.css')}}">
-    <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/vendors/simple-line-icons/css/simple-line-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/vendors/css/vendor.bundle.base.css')}}">
     <link rel="stylesheet" href="{{asset('css/css/style.css')}}">
     <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" />
-    <script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
+    <script src="{{asset('storage/vendors/js/vendor.bundle.base.js')}}"></script>
     <script src="{{asset('js/off-canvas.js')}}"></script>
     <script src="{{asset('js/misc.js')}}"></script>
 </head>
